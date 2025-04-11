d4226e78bc897834cd370b5e73a648e4838a9d22
hyrumjackson <93279082+hyrumjackson@users.noreply.github.com>
2025-04-11 10:03:37 -0600

    Added host info screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import TitleScreen from './pages/TitleScreen';
 import PlayScreen from './pages/PlayScreen';
+import HostInfo from './pages/HostInfo';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<TitleScreen />} />
         <Route path="/play" element={<PlayScreen />} />
+        <Route path="/host" element={<HostInfo />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/HostInfo.tsx
@@ -0,0 +1,52 @@
+import { useNavigate } from 'react-router-dom';
+
+const HostInfo = () => {
+    const navigate = useNavigate();
+
+    return (
+        <div className="page">
+            <h2>Enter Name</h2>
+            <input type="text" placeholder="Your name" />
+            <br />
+            <br />
+            <h2>Select Avatar</h2>
+            <div>
+                <label>
+                    <input type="radio" name="avatar" value="avatar1" />
+                    🐶
+                </label>
+                <label>
+                    <input type="radio" name="avatar" value="avatar2" />
+                    🐱
+                </label>
+                <label>
+                    <input type="radio" name="avatar" value="avatar3" />
+                    🐸
+                </label>
+                <label>
+                    <input type="radio" name="avatar" value="avatar4" />
+                    🐵
+                </label>
+                <label>
+                    <input type="radio" name="avatar" value="avatar5" />
+                    🦊
+                </label>
+                <label>
+                    <input type="radio" name="avatar" value="avatar6" />
+                    🐯
+                </label>
+                <label>
+                    <input type="radio" name="avatar" value="avatar7" />
+                    🐼
+                </label>
+                <label>
+                    <input type="radio" name="avatar" value="avatar8" />
+                    🐧
+                </label>
+            </div>
+            <button onClick={() => navigate('/host-lobby')}>Create Game</button>
+        </div>
+    );
+};
+
+export default HostInfo;
