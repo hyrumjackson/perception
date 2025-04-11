7545fdd5ad452f8d5f11ecfc6fc83c0e8a631f81
hyrumjackson <93279082+hyrumjackson@users.noreply.github.com>
2025-04-11 10:11:35 -0600

    Added host lobby

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import TitleScreen from './pages/TitleScreen';
 import PlayScreen from './pages/PlayScreen';
 import HostInfo from './pages/HostInfo';
+import HostLobby from './pages/HostLobby';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<TitleScreen />} />
         <Route path="/play" element={<PlayScreen />} />
         <Route path="/host" element={<HostInfo />} />
+        <Route path="/host-lobby" element={<HostLobby />} />
       </Routes>
     </Router>
   );
