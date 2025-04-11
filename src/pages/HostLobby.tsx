b161cc67fcb816159229f00dfc0b848b6dce9885
hyrumjackson <93279082+hyrumjackson@users.noreply.github.com>
2025-04-11 16:32:51 -0600

    Added player avatar to lobby

--- src/pages/HostLobby.tsx
+++ src/pages/HostLobby.tsx
@@ -11,7 +11,16 @@ const HostLobby = () => {
             <p>BYU25</p>
             <h2>Players</h2>
             <ul>
-                {player ? <li>{player.name} (Host)</li> : <li>Loading player...</li>}
+                {player ? (
+                    <li>
+                        <span>
+                            {player.avatarId}
+                        </span>
+                        {player.name} (Host)
+                    </li>
+                ) : (
+                    <li>Loading player...</li>
+                )}
             </ul>
             <button onClick={() => navigate('/game-settings')}>Start Game</button>
         </div>
